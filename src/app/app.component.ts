60a0fc83b04013c57442b80932ebe536033cbb2b
GCWehmeyer <gcwehmeyer.10@gmail.com>
2020-11-18 11:04:55 +0200

    Login components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'project';
+  title = 'login';
+  loginClick(event: Event) {
+    console.log('Login clicked', event);
+  }
 }
