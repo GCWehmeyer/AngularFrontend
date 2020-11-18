a7c8501657988084707b903c4d6d846fa3bb68bf
GCWehmeyer <gcwehmeyer.10@gmail.com>
2020-11-18 18:21:59 +0200

    Updates

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -1,17 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-data',
   templateUrl: './data.component.html',
   styleUrls: ['./data.component.scss']
 })
+
 export class DataComponent implements OnInit {
-  uploadFile(event: Event) {
-    console.log('Login clicked', event);
-  }
-  constructor() { }
 
-  ngOnInit(): void {
+  uploadedFiles: Array < File > ;
+  constructor(private http: HttpClient) { }
+  ngOnInit() { }
+
+  fileChange(element) {
+    this.uploadedFiles = element.target.files;
   }
 
+  upload() {
+    let formData = new FormData();
+    for (var i = 0; i < this.uploadedFiles.length; i++) {
+        formData.append("uploads[]", this.uploadedFiles[i], this.uploadedFiles[i].name);
+    }
+    this.http.post('/api/upload', formData)
+    .subscribe((response) => {
+         console.log('response received is ', response);
+    })
+  }
 }
