b410a3b53783863c5cee49adfa8a23eadd66222f
GCWehmeyer <gcwehmeyer.10@gmail.com>
2020-11-19 11:35:17 +0200

    Icon chaged

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  values = '';
+
   /*onKey(event: any) {
     this.values += event.target.value;
   }
