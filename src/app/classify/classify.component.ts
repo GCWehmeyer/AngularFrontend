dcffbd3681592e768893cd4823c39503a5c80bf1
GCWehmeyer <gcwehmeyer.10@gmail.com>
2020-11-21 08:24:55 +0200

    Successful UI

--- src/app/classify/classify.component.ts
+++ src/app/classify/classify.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule} from '@angular/common/http';
 import { ApiserviceService } from '../services/apiservice.service';
 import { Injectable } from '@angular/core';
 
@@ -8,10 +8,32 @@ import { Injectable } from '@angular/core';
   templateUrl: './classify.component.html',
   styleUrls: ['./classify.component.scss']
 })
-export class ClassifyComponent implements OnInit {
 
-  constructor(private http: HttpClientModule, private api: ApiserviceService) { }
+export class ClassifyComponent implements OnInit {
+  fileData: File = null;
+  constructor(private http: HttpClient,  private API: ApiserviceService) { }
 
   ngOnInit(): void {
   }
+
+  fileProgress(fileInput: any): void {
+    this.fileData = <File>fileInput.target.files[0];
+  }
+
+  testing(): any{
+    this.API.getData().subscribe(res => {
+      console.log(res);
+    });
+  }
+
+  onSubmit(): void {
+    const formData = new FormData();
+    formData.append('file', this.fileData);
+    this.http.post('/', formData)
+      .subscribe(res => {
+        console.log(res);
+        alert('SUCCESS !!');
+      });
+  }
+
 }
