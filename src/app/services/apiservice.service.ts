09cb93cda4270c75a69e2cf1b4f47c066c99a8a6
GCWehmeyer <gcwehmeyer.10@gmail.com>
2020-11-22 22:49:40 +0200

    Final UI

--- src/app/services/apiservice.service.ts
+++ src/app/services/apiservice.service.ts
@@ -10,13 +10,17 @@ export class ApiserviceService {
   SERVER_URL = '{Server URL}';
   constructor(private httpClient: HttpClient) { }
 
-  private urlAPI = 'http://localhost:8000';
+  private urlAPI = 'http://localhost:6000';
 
   upload(formData): Observable<any> {
     return this.httpClient.post<any>(this.SERVER_URL, formData, {
       reportProgress: true,
       observe: 'events'
     });
+  }
+
+  sendFile(file: any): Observable<any> {
+    return this.httpClient.post<any>(this.urlAPI + '/api/upload', {file});
   }
 
   getData(): Observable<any> {
